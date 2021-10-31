e7a580235db9f541192b6185c5ea2c11269d15b2
nurhossainfarid <90930368+nurhossainfarid@users.noreply.github.com>
2021-10-31 09:23:05 +0600

    change secure code

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -17,18 +17,18 @@ document.addEventListener("contextmenu", (content) => {
   content.preventDefault();
 });
 document.onkeydown = function (e) {
-  if (event.keyCode == 123 ) {
+  if (event.keyCode === 123 ) {
     return false;
   }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".
+  if (e.ctrlKey && e.shiftKey && e.keyCode === "I".
   charCodeAt(0)) {
     return false;
   }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".
+  if (e.ctrlKey && e.shiftKey && e.keyCode === "J".
   charCodeAt(0)) {
     return false;
   }
-  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+  if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) {
     return false;
   }
 };
